84e5d8bb00369b798f4847ae1865403343ccc8c2
Qanxan <123841379+Qanxan@users.noreply.github.com>
2025-03-02 21:48:04 +0300

    YonlendirmeOdevi
    YonlendirmeOdevi

---
+++ YonlendirmeOdevi/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace YonlendirmeOdevi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnCar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnLeft.Location = new System.Drawing.Point(752, 384);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(50, 50);
+            this.btnLeft.TabIndex = 0;
+            this.btnLeft.Text = "Sol";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnUp.Location = new System.Drawing.Point(808, 328);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(50, 50);
+            this.btnUp.TabIndex = 1;
+            this.btnUp.Text = "Üst";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnRight.Location = new System.Drawing.Point(864, 384);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(50, 50);
+            this.btnRight.TabIndex = 2;
+            this.btnRight.Text = "Sağ";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDown.Location = new System.Drawing.Point(808, 440);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(50, 50);
+            this.btnDown.TabIndex = 3;
+            this.btnDown.Text = "Alt";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnCar
+            // 
+            this.btnCar.BackgroundImage = global::YonlendirmeOdevi.Properties.Resources.pngegg;
+            this.btnCar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCar.Location = new System.Drawing.Point(383, 183);
+            this.btnCar.Name = "btnCar";
+            this.btnCar.Size = new System.Drawing.Size(167, 105);
+            this.btnCar.TabIndex = 4;
+            this.btnCar.UseVisualStyleBackColor = true;
+            this.btnCar.Click += new System.EventHandler(this.btnCar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(926, 496);
+            this.Controls.Add(this.btnCar);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnLeft);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnCar;
+    }
+}
+
---
+++ YonlendirmeOdevi/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YonlendirmeOdevi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            btnCar.Location = new Point(btnCar.Location.X, btnCar.Location.Y-5);
+            btnCar.BackColor = Color.Green;
+        }
+
+        private void btnCar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            btnCar.Location = new Point(btnCar.Location.X, btnCar.Location.Y + 5);
+            btnCar.BackColor= Color.Red;
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            btnCar.Location = new Point(btnCar.Location.X + 5, btnCar.Location.Y);
+            btnCar.BackColor= Color.Blue;
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            btnCar.Location = new Point(btnCar.Location.X - 5, btnCar.Location.Y);
+            btnCar.BackColor=Color.Yellow;
+        }
+    }
+}
